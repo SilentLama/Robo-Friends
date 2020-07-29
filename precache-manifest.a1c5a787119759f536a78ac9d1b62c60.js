58a599526e0d4c50983a3a85df42b8899b77b68a
SilentLama <61166264+SilentLama@users.noreply.github.com>
2020-07-29 20:05:11 +0200

    Updates

---
+++ precache-manifest.a1c5a787119759f536a78ac9d1b62c60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be7ef6627280082e5f7b228a16ea2eb",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "5629e29903776bee648f",
+    "url": "/robo-friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "11d321ffb724e97c7785",
+    "url": "/robo-friends/static/css/main.eb545b38.chunk.css"
+  },
+  {
+    "revision": "5629e29903776bee648f",
+    "url": "/robo-friends/static/js/2.a9c43ba2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends/static/js/2.a9c43ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d321ffb724e97c7785",
+    "url": "/robo-friends/static/js/main.545d5c6d.chunk.js"
+  },
+  {
+    "revision": "1fc1d57fba5bc44bf7e3",
+    "url": "/robo-friends/static/js/runtime-main.efa5b401.js"
+  },
+  {
+    "revision": "632ad65363f8b4e7681bfa49388a3b66",
+    "url": "/robo-friends/static/media/ROBO.632ad653.woff"
+  }
+]);
